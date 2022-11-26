bfc788b7b65af5286f9018a38f1f45dc5dd033d2
Logan Gregg <Logan.Gregg@jacks.sdstate.edu>
2022-11-25 19:27:40 -0600

    Add frontend changes -- ItemList.js page working

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -2,57 +2,56 @@ import {useState, useEffect} from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
 
-const UniversityList = () => {
-    const [universities, setUniversities] = useState([]);
-    const getUniversities = async () => {
-        const response = await axios.get(`http://localhost:5000/universities`);
-        setUniversities(response.data);
+const ItemList = () => {
+    const [items, setItems] = useState([]);
+    const getItems = async () => {
+        const response = await axios.get(`http://localhost:5000/items`);
+        setItems(response.data);
     }
-    const deleteUniversity = async (id) => {
-        await axios.delete(`http://localhost:5000/universities/${id}`);
-        await getUniversities();
+    const deleteItem = async (id) => {
+        await axios.delete(`http://localhost:5000/items/${id}`);
+        await getItems();
     }
     useEffect(() => {
-        getUniversities().then(m => console.log("Successfully retrieved the universities!"));
+        getItems().then(m => console.log("Successfully retrieved the items!"));
     }, []);
     return (
         <main>
-            <table className={"table table-hover table-striped"} aria-label={"Universities table"}>
-                <caption><h1>University List</h1></caption>
+            <table className={"table table-hover table-striped"} aria-label={"Items table"}>
+                <h1>Item List</h1>
                 <thead>
-                <tr>
-                    <th scope={"col"}>Name</th>
-                    <th scope={"col"}>Enrollment Count</th>
-                    <th scope={"col"}>City</th>
-                    <th scope={"col"}>State</th>
-                    <th scope={"col"}>Update</th>
-                    <th scope={"col"}>Delete</th>
-                </tr>
                 </thead>
                 <tbody>
-                {universities.map((university) => (
-                    <tr key={university.id}>
-                        <td>{university.name}</td>
-                        <td>{university.enrollment_count}</td>
-                        <td>{university.state}</td>
-                        <td>{university.city}</td>
+                <tr>
+                    <th scope={"col"}>Name</th>
+                    <th scope={"col"}>Category</th>
+                    <th scope={"col"}>Subcategory</th>
+                    <th scope={"col"}>Use</th>
+                    <th scope={"col"}>Seller ID</th>
+                    <th scope={"col"}>Buy-Now Price</th>
+                    <th scope={"col"}>Delete</th>
+                </tr>
+                {items.map((item) => (
+                    <tr key={item.itemID}>
+                        <td>{item.name}</td>
+                        <td>{item.category}</td>
+                        <td>{item.subcategory}</td>
+                        <td>{item.use}</td>
+                        <td>{item.sellerID}</td>
+                        <td>{item.buyNowPrice}</td>
                         <td>
-                            <Link to={`/edit/${university.id}`} className={"btn btn-secondary"}
-                                aria-label={`Edit ${university.name}`}>Edit</Link>
-                        </td>
-                        <td>
-                            <button onClick={() => deleteUniversity(university.id)} className={"btn btn-secondary"}
-                                    aria-label={`Delete ${university.name}`}>Delete</button>
+                            <button onClick={() => deleteItem(item.itemID)} className={"btn btn-secondary"}
+                                    aria-label={`Delete ${item.name}`}>Delete</button>
                         </td>
                     </tr>
                 ))}
                 </tbody>
             </table>
             <div className={"text-center"}>
-                <Link to={'/add'} className={"btn btn-secondary"}>Add University</Link>
+                <Link to={'/add'} className={"btn btn-secondary"}>Add Item</Link>
             </div>
         </main>
     )
 }
 
-export default UniversityList;
+export default ItemList;
