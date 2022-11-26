515c0b9b4bf4e5c545052d972b1f26627522c7af
Logan Gregg <Logan.Gregg@jacks.sdstate.edu>
2022-11-25 19:27:40 -0600

    Add frontend changes -- ItemList.js page working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import ItemList from "./components/ItemList";
+// import AddUniversity from "./components/AddUniversity";
+// import EditUniversity from "./components/EditUniversity";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <BrowserRouter>
+        <div className={"container"}>
+          <Routes>
+            <Route path={"/"} element={<ItemList/>}/>
+            {/* <Route path={"/add"} element={<AddUniversity/>}/> */}
+            {/* <Route path={"/edit/:id"} element={<EditUniversity/>}/> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
+  )
 }
-
 export default App;
