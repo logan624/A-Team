4267f4a1fd64196faf7cdf19c75c6f89f27ba9de
Logan Gregg <Logan.Gregg@jacks.sdstate.edu>
2022-11-27 21:00:33 -0600

    Bench after Sunday's work

--- frontend/src/components/AddItem.js
+++ frontend/src/components/AddItem.js
@@ -2,56 +2,78 @@ import {useState} from "react";
 import axios from "axios";
 import {Link, useNavigate} from "react-router-dom";
 import {useForm} from "react-hook-form";
+import Dropdown from 'react-bootstrap/Dropdown';
 
-const AddUniversity = () => {
-    const [universityName, setUniversityName] = useState('');
-    const [enrollmentCount, setEnrollmentCount] = useState('');
-    const [universityCity, setCity] = useState('');
-    const [universityState, setState] = useState('');
+const AddItem = () => {
+    const [item_name, setItemName] = useState('');
+    const [category, setCategory] = useState('');
+    const [subcategory, setSubcategory] = useState('');
+    const [use, setUse] = useState('');
+    const [buy_now_price, setBuyNowPrice] = useState('');
     const navigate = useNavigate();
     const {register, handleSubmit, formState: {errors}} = useForm({mode: 'onBlur'});
 
-    const saveUniversity = async () => {
-        await axios.post('http://localhost:5000/universities', {
-            name: universityName,
-            enrollment_count: enrollmentCount,
-            state: universityState,
-            city: universityCity
+    const saveItem = async () => {
+        await axios.post('http://localhost:5000/items', {
+            name: item_name,
+            category: category,
+            subcategory: subcategory,
+            use: use,
+            buyNowPrice: buy_now_price,
+            sellerID: "Jeremy_wade31"
         });
         navigate('/');
     }
     return (
         <main>
-            <form onSubmit={handleSubmit(saveUniversity)} className={"col-md-6 mx-auto"}>
-                <h1 className={"text-center"}>Add University</h1>
+            <form onSubmit={handleSubmit(saveItem)} className={"col-md-6 mx-auto"}>
+                <h1 className={"text-center"}>Add Item</h1>
                 <hr/>
-                <label htmlFor={"universityName"}>University Name:</label>
-                <input type={"text"} className={"form-control"} id={"universityName"} name={"universityName"}
-                       {...register('universityName', {required: true})} placeholder={"Augustana University"}
-                       value={universityName}
-                       onChange={(e) => setUniversityName(e.target.value)} autoFocus
+                <label htmlFor={"item_name"}>Item Name:</label>
+                <input type={"text"} className={"form-control"} id={"item_name"} name={"item_name"}
+                       {...register('item_name', {required: true})} placeholder={"Nike Air Force One's"}
+                       value={item_name}
+                       onChange={(e) => setItemName(e.target.value)} autoFocus
                 />
-                {errors.universityName?.type === 'required' && <small>The university name is required</small>}
-                <label htmlFor={"enrollmentCount"}>University Enrollment Count:</label>
-                <input type={"text"} className={"form-control"} id={"enrollmentCount"} name={"enrollmentCount"}
-                       {...register('enrollmentCount', {required: true})} placeholder={"Ex: 1662"}
-                       value={enrollmentCount} onChange={(e) => setEnrollmentCount(e.target.value)}
+                {errors.category?.type === 'required' && <small>The item name is required</small>}
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Select Category
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item></Dropdown.Item>
+                        <Dropdown.Item></Dropdown.Item>
+                        <Dropdown.Item></Dropdown.Item>
+                        <Dropdown.Item></Dropdown.Item>
+                        <Dropdown.Item></Dropdown.Item>
+                        <Dropdown.Item></Dropdown.Item>
+
+                    </Dropdown.Menu>
+                </Dropdown>
+                {/* <input type={"text"} className={"form-control"} id={"category"} name={"category"}
+                       {...register('category', {required: true})} placeholder={""}
+                       value={category} onChange={(e) => setCategory(e.target.value)}
+                /> */}
+                {errors.category?.type === 'required' && <small>The category is required.</small>}
+                <label htmlFor={"subcategory"}>Subcategory:</label>
+                <input type={"text"} className={"form-control"} id={"subcategory"} name={"subcategory"}
+                       {...register('subcategory', {required: true})} placeholder={""}
+                       value={subcategory} onChange={(e) => setSubcategory(e.target.value)}
                 />
-                {errors.enrollmentCount?.type === 'required' && <small>The enrollment count is required</small>}
-                <label htmlFor={"universityCity"}>City:</label>
-                <input type={"text"} className={"form-control"} id={"universityCity"} name={"universityCity"}
-                       {...register('universityCity', {required: true})} placeholder={"Ex: Sioux Falls"}
-                       value={universityCity} onChange={(e) => setCity(e.target.value)}
+                {errors.use?.type === 'required' && <small>The subcategory is required</small>}
+                <label htmlFor={"use"}>Use:</label>
+                <input type={"text"} className={"form-control"} id={"use"} name={"use"}
+                       {...register('use', {required: true})} placeholder={"Ex: SD"}
+                       value={use} onChange={(e) => setUse(e.target.value)}
                 />
-                {errors.universityCity?.type === 'required' && <small>The city is required</small>}
-                <label htmlFor={"universityState"}>State:</label>
-                <input type={"text"} className={"form-control"} id={"universityState"} name={"universityState"}
-                       {...register('universityState', {required: true})} placeholder={"Ex: SD"}
-                       value={universityState} onChange={(e) => setState(e.target.value)}
+                <label htmlFor={"buy_now_price"}>Buy Now Price:</label>
+                <input type={"text"} className={"form-control"} id={"buy_now_price"} name={"buy_now_price"}
+                       {...register('buy_now_price', {required: true})} placeholder={"Ex: $65.00"}
+                       value={buy_now_price} onChange={(e) => setBuyNowPrice(e.target.value)}
                 />
-                {errors.universityState?.type === 'required' && <small>The state is required</small>}
+                {errors.buy_now_price?.type === 'required' && <small>The buy now price is required</small>}
                 <div className={"text-center"}>
-                    <button className={"btn btn-secondary me-2"}>Add University</button>
+                    <button className={"btn btn-secondary me-2"}>Add Item</button>
                     <Link to={"/"} className={"btn btn-secondary"}>Cancel</Link>
                 </div>
             </form>
@@ -59,4 +81,4 @@ const AddUniversity = () => {
     )
 }
 
-export default AddUniversity;
+export default AddItem;
