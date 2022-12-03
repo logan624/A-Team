7dccee201908303466e2c2d5f96abd94a158d6d9
Logan Gregg <Logan.Gregg@jacks.sdstate.edu>
2022-12-03 17:34:26 -0600

    Home Page Benchmark

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,12 +1,40 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImage,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
 
 const HomePage = () => {
+    const [categorical_recs, setCategoricalRecs] = useState([]);
+    const getCategoricalRecs = async () => {
+        const response = await axios.get(`http://localhost:5000/categorical/recommendations/Jeremy_wade31`);
+        setCategoricalRecs(response.data);
+    }
+
+    // const categorical_recs = getCategoricalRecs();
+    
+    // const [similarRecs, setSimilarRecs] = useState([]);
+    // const getSimilarRecs = async () => {
+    //     const similar_recs = await axios.get(`http://localhost:5000/similar/recommendations/jeremy_wade31`);
+    // }
+    // const [topItemRecs, setTopItemRecs] = useState([]);
+    // const getTopItemRecs = async () => {
+    //     const top_items = await axios.get(`http://localhost:5000/top_item_recommendations`);
+    // }
+    useEffect(() => {
+        getCategoricalRecs().then(m => console.log("Successfully retrieved the items!"));
+    }, []);
+    console.log(JSON.stringify(categorical_recs));
     return (
         <main>
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
@@ -34,7 +62,19 @@ const HomePage = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <h1>HomePage</h1>
+            {/* <h1>${categorical_recs[0].name}</h1> */}
+            {categorical_recs.map((categorical_rec) => (
+                <MDBCard>
+                <MDBCardImage src='' position='top' alt='...' />
+                <MDBCardBody>
+                    <MDBCardTitle>${categorical_rec.name}</MDBCardTitle>
+                    <MDBCardText>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </MDBCardText>
+                    <MDBBtn>Button</MDBBtn>
+                </MDBCardBody>
+                </MDBCard>
+            ))}
         </main>
     )
 }
